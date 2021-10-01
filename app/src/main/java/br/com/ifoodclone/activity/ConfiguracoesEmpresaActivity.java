d772df6197e2d5bb5c83c8b215f73a8061fc59d9
Onkos123 <theylonfeitosa@gmail.com>
2021-10-01 15:05:37 -0300

    company settings

--- app/src/main/java/br/com/ifoodclone/activity/ConfiguracoesEmpresaActivity.java
+++ app/src/main/java/br/com/ifoodclone/activity/ConfiguracoesEmpresaActivity.java
@@ -1,18 +1,51 @@
 package br.com.ifoodclone.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+
 import br.com.ifoodclone.R;
+import br.com.ifoodclone.activity.model.Empresa;
+import br.com.ifoodclone.databinding.ActivityConfiguracoesEmpresaBinding;
+import br.com.ifoodclone.helper.ConfiguracaoFirebase;
+import br.com.ifoodclone.helper.UsuarioFirebase;
 
 public class ConfiguracoesEmpresaActivity extends AppCompatActivity {
+
+    private ActivityConfiguracoesEmpresaBinding binding;
+
+    private static final int SELECAO_GALERIA = 200;
+    private StorageReference storageReference;
+    private String idUsuarioLogado;
+    private String urlImagemSelecionada = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configuracoes_empresa);
+        binding = ActivityConfiguracoesEmpresaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Configurações iniciais
+        inicializarComponentes();
+        storageReference = ConfiguracaoFirebase.getFirebaseStorage();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
         //Configurações Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -20,5 +53,132 @@ public class ConfiguracoesEmpresaActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        binding.imagePerfilEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                );
+                if( i.resolveActivity(getPackageManager()) != null ){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+
+
     }
+
+    public void validarDadosEmpresa(View view){
+
+        //Valida se os campos foram preenchidos
+        String nome = binding.editEmpresaNome.getText().toString();
+        String taxa = binding.editEmpresaTaxa.getText().toString();
+        String categoria = binding.editEmpresaCategoria.getText().toString();
+        String tempo = binding.editEmpresaTempo.getText().toString();
+
+        if( !nome.isEmpty()){
+            if( !taxa.isEmpty()){
+                if( !categoria.isEmpty()){
+                    if( !tempo.isEmpty()){
+
+                        Empresa empresa = new Empresa();
+                        empresa.setIdUsuario( idUsuarioLogado );
+                        empresa.setNome( nome );
+                        empresa.setPrecoEntrega( Double.parseDouble(taxa) );
+                        empresa.setCategoria(categoria);
+                        empresa.setTempo( tempo );
+                        empresa.setUrlImagem( urlImagemSelecionada );
+                        empresa.salvar();
+                        finish();
+
+                    }else{
+                        exibirMensagem("Digite um tempo de entrega");
+                    }
+                }else{
+                    exibirMensagem("Digite uma categoria");
+                }
+            }else{
+                exibirMensagem("Digite uma taxa de entrega");
+            }
+        }else{
+            exibirMensagem("Digite um nome para a empresa");
+        }
+
+    }
+
+    private void exibirMensagem(String texto){
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try {
+
+                switch (requestCode) {
+                    case SELECAO_GALERIA:
+                        Uri localImagem = data.getData();
+                        imagem = MediaStore.Images
+                                .Media
+                                .getBitmap(
+                                        getContentResolver(),
+                                        localImagem
+                                );
+                        break;
+                }
+
+                if( imagem != null){
+
+                    binding.imagePerfilEmpresa.setImageBitmap( imagem );
+
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    StorageReference imagemRef = storageReference
+                            .child("imagens")
+                            .child("empresas")
+                            .child(idUsuarioLogado + "jpeg");
+
+                    UploadTask uploadTask = imagemRef.putBytes( dadosImagem );
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ConfiguracoesEmpresaActivity.this,
+                                    "Erro ao fazer upload da imagem",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            urlImagemSelecionada = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                            Toast.makeText(ConfiguracoesEmpresaActivity.this,
+                                    "Sucesso ao fazer upload da imagem",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private void inicializarComponentes(){
+
+    }
+
 }
+
